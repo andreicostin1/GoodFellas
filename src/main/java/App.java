4350d58b05e905557c7600548835f55474f2e689
nizhipeng <darren.nee@outlook.com>
2021-05-09 23:07:45 +0800

    Add an App Class to eliminate the inoperable bug in Jetbrains Idea IDE

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+package main.java;
+
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(Main.class,args);
+    }
+}
